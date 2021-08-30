792bc90f0bc0d907b9a58112d63108a2a673ebfc
adonaifariasdev <oficialadonai@hotmail.com>
2021-08-30 01:44:21 -0300

    Adicao de Exercicios

---
+++ Mundo 1/ex002.py
@@ -0,0 +1,3 @@
+nome = input('Digite seu nome: ')
+print('Seja muito bem vindo, ', nome)
+print('É um prazer te conhecer, {}!'.format(nome))
---
+++ Mundo 1/ex005.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um numero: '))
+a = num - 1
+s = num + 1
+print('{} antecessor é: {}.'.format(num, a))
+print('{} sucessor é: {}.'.format(num, s))
