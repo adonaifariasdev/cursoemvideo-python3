f6aa74a1f9ada0c7b5470751461d21627b6faa44
Adonai Farias <89660994+adonaifariasdev@users.noreply.github.com>
2021-10-27 01:51:11 -0400

    Update ex050.py

--- Mundo 2/ex050.py
+++ Mundo 2/ex050.py
@@ -1,4 +1,5 @@
-# Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares.
+# Desenvolva um programa que leia seis números inteiros e 
+# mostre a soma apenas daqueles que forem pares.
 # Se o valor digitado for ímpar, desconsidere-o.
 soma = 0
 cont = 0
