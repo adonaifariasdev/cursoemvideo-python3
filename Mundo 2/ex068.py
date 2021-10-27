aca343dfa99284ae62f8ace2b941945383cf6a82
Adonai Farias <89660994+adonaifariasdev@users.noreply.github.com>
2021-10-27 02:00:49 -0400

    Update ex068.py

--- Mundo 2/ex068.py
+++ Mundo 2/ex068.py
@@ -12,8 +12,10 @@ while True:
     jogador = int(input('Diga um valor: '))
     computador = randint(0, 10)
     soma = jogador + computador
-    # opcao = ' ' -> na situaçao se for colocar o comando While, conforme resolução do Guanabara
-    opcao = str(input('Par ou ÍMPAR? [P/I] ')).upper().strip()[0] # Se colocar um while not in 'PI': -> se qualquer outro caractere digitado voltará a perguntar
+    # opcao = ' ' -> na situaçao se for colocar o comando While, conforme 
+    #resolução do Guanabara
+    opcao = str(input('Par ou ÍMPAR? [P/I] ')).upper().strip()[0] # Se colocar
+    #um while not in 'PI': -> se qualquer outro caractere digitado voltará a perguntar
     print('-'*30)
     if soma % 2 == 0:
         print(f'Você jogou {jogador} e o computador {computador}. Total de {soma} DEU PAR')
