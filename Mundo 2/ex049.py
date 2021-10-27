ec8d251125b6a05cf91348678e246f0e3775932c
Adonai Farias <89660994+adonaifariasdev@users.noreply.github.com>
2021-10-27 01:50:44 -0400

    Update ex049.py

--- Mundo 2/ex049.py
+++ Mundo 2/ex049.py
@@ -1,4 +1,5 @@
-# Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+# Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher,
+# só que agora utilizando um laço for.
 num = int(input('Deseja ver a tabuada de qual número: '))
 for c in range(1, 11):
     print('{} x {:2} = {}'.format(num, c, num * c))
