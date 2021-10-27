fb30be7e83631292d2d5e6bfd35f244cf47ff4b0
Adonai Farias <89660994+adonaifariasdev@users.noreply.github.com>
2021-10-27 01:52:33 -0400

    Update ex051.py

--- Mundo 2/ex051.py
+++ Mundo 2/ex051.py
@@ -1,4 +1,5 @@
-# Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+# Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final,
+# mostre os 10 primeiros termos dessa progressão.
 
 print('='*40)
 print('{:^40}'.format('10 TERMOS DE UMA PA'))
