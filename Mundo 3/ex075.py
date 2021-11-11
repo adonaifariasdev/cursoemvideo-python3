f9563d967ed31ed1fd0b5153d7ea37b724617063
Adonai Farias <89660994+adonaifariasdev@users.noreply.github.com>
2021-11-10 22:32:50 -0400

    Update ex075.py

--- Mundo 3/ex075.py
+++ Mundo 3/ex075.py
@@ -1,4 +1,5 @@
-# Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
+# Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. 
+# No final, mostre:
 # A) Quantas vezes apareceu o valor 9.
 # B) Em que posição foi digitado o primeiro valor 3.
 # C) Quais foram os números pares.
@@ -17,7 +18,8 @@ for c in range(1, 5):
     valores = (n1, n2, n3, n4)
 print(f'Você digitou os valores {valores}')
 print(f'O valor 9 apareceu {valores.count(9)} vezes')
-print(f'O valor 3 apareceu na {valores.index(3)+1}a. posição') # Dando erro pois tem que fazer acondição if 3 in valores como no ex075a
+print(f'O valor 3 apareceu na {valores.index(3)+1}a. posição') # Dando erro pois tem que
+#fazer acondição if 3 in valores como no ex075a
 print(f'Os valores pares digitados foram ', end='')
 for c in valores:
     if c % 2 == 0:
