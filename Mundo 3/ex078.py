f216315fedef42f2c6b9eb0ab49c6d66c7f36575
Adonai Farias <89660994+adonaifariasdev@users.noreply.github.com>
2021-11-10 22:35:29 -0400

    Update ex078.py

--- Mundo 3/ex078.py
+++ Mundo 3/ex078.py
@@ -1,5 +1,6 @@
 # Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
-# No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
+# No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas
+# posições na lista.
 
 valores = []
 for c in range(0, 5):
