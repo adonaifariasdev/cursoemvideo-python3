218059c7aca2ec506c7647b2c33625ddef53d91b
Adonai Farias <89660994+adonaifariasdev@users.noreply.github.com>
2021-11-10 22:31:30 -0400

    Update ex073.py

--- Mundo 3/ex073.py
+++ Mundo 3/ex073.py
@@ -1,4 +1,5 @@
-# Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol,
+# Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro
+# de Futebol,
 # na ordem de colocação. Depois mostre:
 # a) Os 5 primeiros times.
 # b) Os últimos 4 colocados.
