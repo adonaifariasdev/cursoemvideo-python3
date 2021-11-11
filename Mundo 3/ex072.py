d3b92a8c031e58d76f68f478a273016cae56087a
Adonai Farias <89660994+adonaifariasdev@users.noreply.github.com>
2021-11-10 22:29:37 -0400

    Update ex072.py

--- Mundo 3/ex072.py
+++ Mundo 3/ex072.py
@@ -1,8 +1,10 @@
-# Crie um programa que tenha uma Tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
+# Crie um programa que tenha uma Tupla totalmente preenchida com uma contagem por extenso,
+# de zero até vinte.
 # Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 
-'''extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
-           'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenve', 'vinte')'''
+'''extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove',
+           'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito',
+           'dezenve', 'vinte')'''
 # FORMA DO GUANABARA
 cont =  ('zero', 'um', 'dois', 'três', 'quatro',
          'cinco', 'seis', 'sete', 'oito', 'nove',
